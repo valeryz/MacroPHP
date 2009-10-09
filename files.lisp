bd551fb6c45d137c73337e00fe4c727f0faa02f7
Valeriy Zamarayev <valeriy.zamarayev@gmail.com>
2009-10-09 22:37:11 +0300

    improve write-php-file and php-file interface

--- files.lisp
+++ files.lisp
@@ -8,24 +8,24 @@
 (defun rootpath (path)
   (merge-pathnames (parse-namestring path) *document-root*))
 
-(defun write-php-file (pathname &rest code)
+(defun write-php-file (pathname code)
   (with-open-file (out pathname
 		       :direction :output :if-exists :supersede)
     (write-string "<?php" out)
     (terpri out)
-    (phpize out (cons 'progn code))
+    (phpize out code)
     (fresh-line out)
     (write-string "?>" out)
     (terpri out)))
 
-(defun write-js-file (pathname &rest code)
+(defun write-js-file (pathname code)
   (with-open-file (out pathname
 		       :direction :output :if-exists :supersede)
-	 (parenscript:compile-script (list 'progn code) :output-stream out)
+	 (parenscript:compile-script code :output-stream out)
 	 (fresh-line out)))
 
 (defmacro php-file (path &body code)
-  `(apply #'write-php-file ,(rootpath path) ',code))
+  `(write-php-file ,(rootpath path) ',(cons 'progn code)))
 
 (defmacro js-file (path &body code)
-  `(apply #'write-js-file ,(rootpath path) ',code))
+  `(write-js-file ,(rootpath path) ',(cons 'progn code)))
